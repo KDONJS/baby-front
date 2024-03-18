aa18676277c6d8acd3ea0099987ab595867105ba
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-03-17 21:42:38 -0500

    se agrego musica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'babyShower';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
+
+  ngAfterViewInit() {
+    this.playAudio();
+  }
+
+  playAudio() {
+    const player = this.audioPlayer.nativeElement;
+    player.volume = 0.5; // Ajusta el volumen al 50%
+    player.play().catch(error => console.error('La auto reproducción falló', error));
+  }
 }
